929db41ab3099379d7af53973001a9207d042b27
AlexBurkh <alex.burkh@yandex.ru>
2022-09-13 23:45:22 +0300

    Сделаны проверки в contact_book, укорочены команды

--- PhoneBook/bot.py
+++ PhoneBook/bot.py
@@ -18,16 +18,16 @@ class Bot():
         answer = self._Control.load_menu()
         await update.message.reply_text(answer)
     
-    async def print_book(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+    async def print(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         answer = self._Control.load_print_book()
         await update.message.reply_text(answer)
 
-    async def add_contact(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+    async def add(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         contact_data = update.message.text.split()
         answer = self._Control.add_contact(contact_data[1], contact_data[2], contact_data[3], contact_data[4])
         await update.message.reply_text(answer)
 
-    async def delete_contact(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+    async def delete(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         msg = update.message.text.split()
         answer = self._Control.delete_contact(msg[1])
         await update.message.reply_text(answer)
@@ -56,9 +56,9 @@ class Bot():
         self.__app = ApplicationBuilder().token(self.__token).build()
         self.__app.add_handler(CommandHandler("start", self.start))
         self.__app.add_handler(CommandHandler("menu", self.menu))
-        self.__app.add_handler(CommandHandler("add_contact", self.add_contact))
-        self.__app.add_handler(CommandHandler("print_book", self.print_book))
-        self.__app.add_handler(CommandHandler("delete_contact", self.delete_contact))
+        self.__app.add_handler(CommandHandler("add", self.add))
+        self.__app.add_handler(CommandHandler("print", self.print))
+        self.__app.add_handler(CommandHandler("delete", self.delete))
         self.__app.add_handler(CommandHandler("search_by_id", self.search_by_id))
         self.__app.add_handler(CommandHandler("search_by_surname", self.search_by_surname))
         self.__app.add_handler(CommandHandler("import_contacts", self.import_contacts))
