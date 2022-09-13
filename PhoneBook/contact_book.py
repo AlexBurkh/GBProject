0a4e1fc755d6a6b4987ccad638d4f1cf1e847c56
AlexBurkh <alex.burkh@yandex.ru>
2022-09-13 23:22:08 +0300

    Повышение безопасности contact_book путем добавления проверок

--- PhoneBook/contact_book.py
+++ PhoneBook/contact_book.py
@@ -15,35 +15,40 @@ class contact_book:
                           patronymic : str, 
                           surname    : str, 
                           number     : str):
-        self.__contacts.append(contact(self.__next_id, name, patronymic, surname, number))
-        self.__next_id += 1
-    
+        if self.check_contact_data(name, patronymic, surname, number):
+            self.__contacts.append(contact(self.__next_id, name, patronymic, surname, number))
+            self.__next_id += 1
+            
     def _import_contact(self, id : str, 
                              name : str, 
                              patronymic : str, 
                              surname : str, 
                              number : str):
-        self.__contacts.append(contact(id, name, patronymic, surname, number))
-        self.__next_id = id + 1
+        if self.check_contact_data(name, patronymic, surname, number, id = id):
+            self.__contacts.append(contact(id, name, patronymic, surname, number))
+            self.__next_id = id + 1
 
     def import_contact_list(self, contact_list):
-        self.__contacts = []
-        self.__next_id = 0
-        for contact in contact_list:
-            self._import_contact(contact.id, contact.name, contact.patronymic, contact.surname, contact.number)
+        if self.check_text(contact_list):
+            self.__contacts = []
+            self.__next_id = 0
+            for contact in contact_list:
+                self._import_contact(contact.id, contact.name, contact.patronymic, contact.surname, contact.number)
     
     def get_by_id(self, id : int):
         result = []
-        for item in self.__contacts:
-            if item.id == id:
-                result.append(item)
+        if self.check_text(id):
+            for item in self.__contacts:
+                if item.id == id:
+                    result.append(item)
         return result
 
     def get_by_surname(self, surname : str):
         result = []
-        for item in self.__contacts:
-            if item.surname == surname:
-                result.append(item)
+        if self.check_text(surname):
+            for item in self.__contacts:
+                if item.surname == surname:
+                    result.append(item)
         return result
 
     def edit_contact(self, id         : int, 
@@ -55,18 +60,20 @@ class contact_book:
         item.edit(name, patronymic, surname, number)
 
     def delete_contact(self, id : int):
-        flag = False
-        for item in self.__contacts:
-            if item.id == id:
-                self.__contacts.remove(item)
-                flag = True
-        for item in self.__contacts[id:]:
-            item.id -= 1
-        if len(self.__contacts) != 0:
-            self.__next_id = self._current_max_id() + 1
-        else:
-            self.__next_id = 0
-        return flag
+        if self.check_text(id):
+            flag = False
+            for item in self.__contacts:
+                if item.id == id:
+                    self.__contacts.remove(item)
+                    flag = True
+            for item in self.__contacts[id:]:
+                item.id -= 1
+            if len(self.__contacts) != 0:
+                self.__next_id = self._current_max_id() + 1
+            else:
+                self.__next_id = 0
+            return flag
+        return False
     
     def get_sorted(self):
         return sorted([item for item in self.__contacts], key = lambda row: (row.surname,
@@ -75,3 +82,13 @@ class contact_book:
 
     def get_unsorted(self):
         return self.__contacts
+
+    def check_text(self, input):
+        if input != None and input != "" and input != []:
+            return True
+        return False
+
+    def check_contact_data(self, name, patronymic, surname, number, id = 0):
+        if self.check_input(name) and self.check_input(patronymic) and self.check_input(surname) and self.check_input(number) and id >= 0:
+            return True
+        return False
